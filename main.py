e6d539f720f9719cc42dfa57a2abe9a5f74db4aa
arokiaja <rajesh.jayaraj@protonmail.com>
2024-03-04 23:08:15 +0000

    Third Version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 user_input = input("Please input your name: ")
-Message = f"Welcome {user_input}"
+user_location = input("Where are you from? ")
+Message = f"Welcome {user_input} from {user_location}"
 print(message)
